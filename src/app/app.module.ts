ad2bbbb09dc2aa914719d48684dc7a6064766e9e
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-11-16 08:34:59 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LieuComponent } from './composants/lieu/lieu.component';
+import { Exercice1Component } from './pages/exercice1/exercice1.component';
+import { Exercice2Component } from './pages/exercice2/exercice2.component';
+import { Exercice3Component } from './pages/exercice3/exercice3.component';
+import { Exercice4Component } from './pages/exercice4/exercice4.component';
+import { Exercice5Component } from './pages/exercice5/exercice5.component';
+import { Exercice6Component } from './pages/exercice6/exercice6.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LieuComponent,
+    Exercice1Component,
+    Exercice2Component,
+    Exercice3Component,
+    Exercice4Component,
+    Exercice5Component,
+    Exercice6Component
   ],
   imports: [
     BrowserModule,
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -51,6 +55,10 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
